d7a13494f414162ef051104701ff3337447937f1
adamw <adam@warski.org>
2015-05-22 10:50:30 +0200

    Akka merge impl - not working

--- src/main/scala/com/softwaremill/streams/MergeSortedStreams.scala
+++ src/main/scala/com/softwaremill/streams/MergeSortedStreams.scala
@@ -1,8 +1,16 @@
 package com.softwaremill.streams
 
+import akka.actor.ActorSystem
+import akka.stream.FanInShape.Name
+import akka.stream._
+import akka.stream.scaladsl.{FlexiMerge, Sink, Source, FlowGraph}
+import akka.stream.scaladsl.FlowGraph.Implicits._
 import org.scalacheck.{Prop, Gen, Properties}
 
+import scala.concurrent.Await
 import scalaz.stream.{Tee, tee, Process}
+
+import scala.concurrent.duration._
 
 trait MergeSortedStreams {
   def merge[T: Ordering](l1: List[T], l2: List[T]): List[T]
@@ -10,7 +18,19 @@ trait MergeSortedStreams {
 
 object AkkaStreamsMergeSortedStreams extends MergeSortedStreams {
   def merge[T: Ordering](l1: List[T], l2: List[T]): List[T] = {
-    ???
+    val out = Sink.fold[List[T], T](Nil) { case (l, e) => l.+:(e)}
+
+    val g = FlowGraph.closed(out) { implicit builder => sink =>
+      val merge = builder.add(new SortedMerge[T])
+
+      Source(l1) ~> merge.in0
+      Source(l2) ~> merge.in1
+                    merge.out ~> sink.inlet
+    }
+
+    implicit val system = ActorSystem()
+    implicit val mat = ActorFlowMaterializer()
+    try Await.result(g.run(), 1.hour).reverse finally system.shutdown()
   }
 }
 
@@ -47,4 +67,52 @@ object MergeSortedStreamsRunner extends Properties("MergeSortedStreams") {
 
   addPropertyFor("scalaz", ScalazStreamsMergeSortedStreams)
   addPropertyFor("akka", AkkaStreamsMergeSortedStreams)
+}
+
+class SortedMerge[T: Ordering] extends FlexiMerge[T, FanInShape2[T, T, T]](
+  new FanInShape2(Name("SortedMerge")), OperationAttributes.name("SortedMerge")) {
+
+  import akka.stream.scaladsl.FlexiMerge._
+
+  override def createMergeLogic(s: FanInShape2[T, T, T]) = new MergeLogic[T] {
+    private var activeInputs = Set(s.in0, s.in1)
+    private var outstanding: Option[T] = None
+
+    override def initialCompletionHandling = CompletionHandling(
+      onUpstreamFinish = (ctx, input) => {
+        activeInputs = activeInputs.filterNot(_ == input)
+
+        // TODO: we should emit the outstanding element, if any
+        // see https://github.com/akka/akka/issues/16753
+
+        activeInputs.headOption match {
+          case Some(active) => echoInputState(active)
+          case None => ctx.finish(); SameState
+        }
+      },
+      onUpstreamFailure = (ctx, input, cause) => { ctx.fail(cause); SameState })
+
+    override def initialState = State[T](Read(s.in0)) { (ctx, input, l) =>
+      outstanding = Some(l)
+      nextRight(l)
+    }
+
+    def nextLeft(r: T)  = State[T](Read(s.in0)) { (ctx, input, l) => next(ctx, l, r) }
+    def nextRight(l: T) = State[T](Read(s.in1)) { (ctx, input, r) => next(ctx, l, r) }
+
+    def next(ctx: MergeLogicContext, l: T, r: T): State[T] = if (implicitly[Ordering[T]].lt(l, r)) {
+      ctx.emit(l)
+      outstanding = Some(r)
+      nextLeft(r)
+    } else {
+      ctx.emit(r)
+      outstanding = Some(l)
+      nextRight(l)
+    }
+
+    def echoInputState(input: Inlet[T]) = State[T](Read(input)) { (ctx, _, el) =>
+      ctx.emit(el)
+      SameState
+    }
+  }
 }
